a037425dbe9514041ae0668d4a3d651b7796c497
Mark Craft <markshocraft@gmail.com>
2016-11-13 01:07:33 -0500

    Update to Chrome extension

--- extension/myScript.js
+++ extension/myScript.js
@@ -1,78 +1,77 @@
+/**
+ * @file myScript.js
+ * Script to analyze Facebook feed;
+ *
+ * @author Mark Craft, Qinglin Chen
+ * @date Fall 2016
+ */
+
+var feeds = new Set();
+
 (function(document) {
 
-var arr = [];
+/**
+ * Http request to fbserve.herokuapp.com.
+ *
+ * @param the text or url to send to the server.
+ */
+function httpGet(input) {
 
+	var server = "https://fbserve.herokuapp.com/";
+	var theUrl = "https://api.wordnik.com/v4/word.json/javascript/";
+	//var theUrl = server+"?content="+"\""+input+"\"";
+	
+	fetch(theUrl).then(function(res)
+		{ return res.text(); }).then(function(text)
+		{ console.log(text);});
+
+}
+
+/**
+ * Receive each Facebook post and analyze texts, urls, pics for validity.
+ * Refreshes every second.
+ *
+ */
 setInterval(function() {
 
-	/*
-	var link = document.getElementsByClassName('_52c6');//document.getElementsByTagName("a");
+	var test = document.getElementsByClassName('_4-u2 mbm _5v3q _4-u8');
 
-	for(var i=0; i<link.length; i++) {
-		var url = link[i].href;
-		if(arr[url] != 1) {
-			arr.push(url);
-			arr[url] = 1;
-			console.log(url);
+	for(var i=0; i<test.length; i++) {
+
+		var data = test[i];
+
+		// Check if feed needs to be modified
+
+		if(!feeds.has(data)) {
+			feeds.add(data);
+
+			// Send server requests
+
+			var shared = test[i].querySelector('._52c6');
+			if(shared != null && shared.href!=undefined)
+				httpGet(shared.href);
+
+			var text = test[i].querySelector('._5pbx.userContent');
+			if(text != null && text.textContent!=undefined)
+				httpGet(text.textContent);
+
+			var picComment = test[i].querySelector('.uiScaledImageContainer._4-ep');
+			if(picComment != null && picComment.src!=undefined)
+				httpGet(picComment.src);
+
+			var picPost = test[i].querySelector('._46-h _517g');
+			if(picPost != null && picPost.src!=undefined)
+				httpGet(picPost.src);
+
+			// Add indication for validity
+
+			var div = document.createElement('div');
+			data.appendChild(div);
+			div.innerHTML = 'TEST';
+			div.style = "front-weight:bold; padding: 3px;color:red; position:absolute; top: 10px; right: 30px; background: #3b5998"
+
 		} else {
-			//console.log("link in hash table");
-		}
-	}
-	
-	var text = document.getElementsByClassName('_5pbx userContent');
-
-	for(var i=0; i<text.length; i++) {
-		var txt = text[i].textContent;
-		if(arr[txt] != 1) {
-			arr.push(txt);
-			arr[txt] = 1;
-			console.log(txt);
-		} else {
-			//console.log("text in hash table");
-		}
-	}
-	*/
-    
-    /*
-	var picComment = document.getElementsByClassName('uiScaledImageContainer _4-ep');
-
-	for(var i=0; i<picComment.length; i++) {
-		var picture = picComment[i].getElementsByTagName('img');
-		var pc = picture[0].src;
-		if(arr[pc] != 1) {
-			arr.push(pc);
-			arr[pc] = 1;
-			console.log(pc);
-		} else {
-			//console.log("pic in hash table");
-		}
-	}
-
-	var picPost = document.getElementsByClassName('_46-h _517g');
-
-	for(var i=0; i<picPost.length; i++) {
-		var picture2 = picPost[i].getElementsByTagName('img');
-		var pc2= picture2[0].src;
-		if(arr[pc2] != 1) {
-			arr.push(pc2);
-			arr[pc2] = 1;
-			console.log(pc2);
-		} else {
-			//console.log("pic in hash table");
-		}
-	}
-	*/
-
-	var test = document.getElementsByClassName('userContentWrapper _5pcr');
-
-	for(var i=0; i<picComment.length; i++) {
-		//var picture = picComment[i].getElementsByTagName('img');
-		//var pc = picture[0].src;
-		if(arr[test] != 1) {
-			arr.push(test);
-			arr[test] = 1;
-			console.log(test);
-		} else {
-			//console.log("pic in hash table");
+			//console.log("have feed");
 		}
 	}
 
