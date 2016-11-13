0cfe49da3ec6c0933afdf8224766279edc85de9c
Mark Craft <markshocraft@gmail.com>
2016-11-13 05:54:18 -0500

    Update Chrome Extension

--- extension/myScript.js
+++ extension/myScript.js
@@ -1,3 +1,4 @@
+
 /**
  * @file myScript.js
  * Script to analyze Facebook feed;
@@ -15,16 +16,34 @@ var feeds = new Set();
  *
  * @param the text or url to send to the server.
  */
-function httpGet(input) {
+function httpGet(input, type, data) {
 
-	var server = "https://fbserve.herokuapp.com/";
-	var theUrl = "https://api.wordnik.com/v4/word.json/javascript/";
-	//var theUrl = server+"?content="+"\""+input+"\"";
+	var server = "https://trustfb.herokuapp.com/";
+	var contents = "?content=";
+	//var theUrl = "https://api.wordnik.com/v4/word.json/javascript/";
+	var theUrl = server+type+contents+input;
+	theUrl = theUrl.replace("&", "^");
 	
-	fetch(theUrl).then(function(res)
-		{ return res.text(); }).then(function(text)
-		{ console.log(text);});
+	//test = document.body.innerHTML;
 
+	fetch(theUrl)
+		.then(function(res)
+		{ return res.text(); })
+		.then(function(text)
+		{
+			console.log(text);
+			
+			var div = document.createElement('div'),
+				button = Ladda.create(div);
+			data.appendChild(div);
+			div.innerHTML = text;
+			div.style = "front-weight:bold; padding: 3px;color:red; position:absolute; top: 3px; right: 30px; background: #3b5998";
+			//var l = Ladda.create(div),
+			/*
+			div.innerHTML = text;
+			div.style = "front-weight:bold; padding: 3px;color:red; position:absolute; top: 10px; right: 30px; background: #3b5998";
+			*/
+		});
 }
 
 /**
@@ -33,7 +52,7 @@ function httpGet(input) {
  *
  */
 setInterval(function() {
-
+	
 	var test = document.getElementsByClassName('_4-u2 mbm _5v3q _4-u8');
 
 	for(var i=0; i<test.length; i++) {
@@ -47,28 +66,32 @@ setInterval(function() {
 
 			// Send server requests
 
+			var statement = "";
+
 			var shared = test[i].querySelector('._52c6');
 			if(shared != null && shared.href!=undefined)
-				httpGet(shared.href);
+				httpGet(shared.href, "", data);
 
 			var text = test[i].querySelector('._5pbx.userContent');
 			if(text != null && text.textContent!=undefined)
-				httpGet(text.textContent);
+				httpGet(text.textContent, "", data);
 
 			var picComment = test[i].querySelector('.uiScaledImageContainer._4-ep');
 			if(picComment != null && picComment.src!=undefined)
-				httpGet(picComment.src);
+				httpGet(picComment.src, "image", data);
 
-			var picPost = test[i].querySelector('._46-h _517g');
+			var picPost = test[i].querySelector('._46-h._517g');
 			if(picPost != null && picPost.src!=undefined)
-				httpGet(picPost.src);
+				httpGet(picPost.src, "image", data);
 
 			// Add indication for validity
 
+			/*
 			var div = document.createElement('div');
 			data.appendChild(div);
-			div.innerHTML = 'TEST';
+			div.innerHTML = statement;
 			div.style = "front-weight:bold; padding: 3px;color:red; position:absolute; top: 10px; right: 30px; background: #3b5998"
+			*/
 
 		} else {
 			//console.log("have feed");
