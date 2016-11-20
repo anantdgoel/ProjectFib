8d58ac94271cbdbc4dc17db6299c61fd9f28d904
thundergolfer <jonathon.bel.melbourne@gmail.com>
2016-11-20 17:07:52 +1100

    Code documenting, commenting, and code cleanup.

--- backend/imageverify.py
+++ backend/imageverify.py
@@ -3,57 +3,53 @@ import json
 from pprint import pprint
 import urllib
 
-def detectAdultContent(body):
-    dp1 = ""
-    dp2 = ""
+def no_adult_content(body):
+    """
+    Use Microsoft's Project Oxford Computer Vision API to detect Adult/NSFW content in images.
+    Returns True if content is Safe For Work (SFW), and False otherwise.
+    """
+    isAdult = False
+    isRacy = False
     headers = {'Content-Type': 'application/json', 'Ocp-Apim-Subscription-Key': '92e7b0d9a88a4a6495c5b40481cbe81e',}
     params = urllib.parse.urlencode({'visualFeatures': 'Adult', 'language': 'en',})
     #body = "{\"url\":\"http://www.gettyimages.ca/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg\"}"
+    microsoft_project_oxford_endpoint = 'api.projectoxford.ai'
     try:
-        conn = http.client.HTTPSConnection('api.projectoxford.ai')
+        conn = http.client.HTTPSConnection(microsoft_project_oxford_endpoint)
         conn.request("POST", "/vision/v1.0/analyze?%s" % params, body, headers)
         response = conn.getresponse()
         data = response.read()
         data = json.loads(data.decode("utf-8"))
-        #print("isAdultContent")
-        #print(data)
-        dp1 = data['adult']['isAdultContent']
-        #print("isRacyContent")
-        dp2 = (data['adult']['isRacyContent'])
-        
-        #print(data)
+        isAdult = data['adult']['isAdultContent']
+        isRacy =  data['adult']['isRacyContent']
         conn.close()
     except Exception as e:
         print("[Errno {0}] {1}".format(e.errno, e.strerror))
-    #print(dp1)
-    #print(dp2)
-    if dp1 == False & dp2 == False:
-        #print('True')
+    if not isAdult and not isRacy:
         return(True)
     else:
-        #print('False')
         return(False)
-    
 
-
-
-#detectAdultContent("{\"url\":\"http://www.gettyimages.ca/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg\"}")
+#no_adult_content("{\"url\":\"http://www.gettyimages.ca/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg\"}")
 #link = 'dkakasdkjdjakdjadjfalskdjfalk'
 
-def twitterpresentorno(link):
+# TODO this function's variable names are pretty confusing, and there's not enough informative
+#      commenting for a 80 line function. The function is likely still too long.
+def twitter_present(link):
     import http.client, urllib.request, urllib.parse, urllib.error, base64
     import re
+    from twython import Twython # pip install twython
+    import time # standard lib
     u = 0
     return1 = 0
-    thestring = "text1\ntext2\nhttp://url.com/bla1/blah1/\ntext3\ntext4\nhttp://url.com/bla2/blah2/\ntext5\ntext6"
     twitterorno = 0
     thetwittertext = ""
     twitterusers = []
-    dk = "abc"
-    #print('here')
-    from twython import Twython # pip install twython
-    import time # standard lib
-    twitter = Twython('WwA5L9U5PCqbnlfblKwKF0LEo','ezUnYWFGyoOzeW68Au7MWLbYvX8ashIYWMEPtOnMqmxQgzXlRu','136941431-Fwtibpyy072k8bqnoJyvLyrid69ZnUIYqbgPGiFr','mtkQ6H6XFElMmp6YV3fLlwx6tPBsZLxt39VWvAJ5H3EJY')
+    twitter = Twython('WwA5L9U5PCqbnlfblKwKF0LEo',
+                      'ezUnYWFGyoOzeW68Au7MWLbYvX8ashIYWMEPtOnMqmxQgzXlRu',
+                      '136941431-Fwtibpyy072k8bqnoJyvLyrid69ZnUIYqbgPGiFr',
+                      'mtkQ6H6XFElMmp6YV3fLlwx6tPBsZLxt39VWvAJ5H3EJY') # TODO label these function arguments
+
     headers = {'Content-Type': 'application/json','Ocp-Apim-Subscription-Key': '92e7b0d9a88a4a6495c5b40481cbe81e',}
     params = urllib.parse.urlencode({'language': 'unk','detectOrientation ': 'true',})
     body = "{\"url\":\"" + link + "\"}"
@@ -63,22 +59,18 @@ def twitterpresentorno(link):
         response = conn.getresponse()
         data = response.read()
         data = json.loads(data.decode("utf-8"))
-        
-        #print(data)
-        #print(len(data))
+
         if len(data) == 3:
             if data['orientation'] == 'NotDetected':
                 return1 = -1
                 u = 1
-                #print("yes")
         else:
-            #print("yo")
             for p in data['regions'][0]['lines']:
                 for x in p['words']:
-                    thetwittertext = thetwittertext + " " + x['text'] 
+                    thetwittertext = thetwittertext + " " + x['text']
                     if '@' in x['text']:
                         twitterusers.append(x['text'])
-                        twitterorno = 1    
+                        twitterorno = 1
                     elif x['text'].lower() in ["tweet","retweets"]:
                         twitterorno = 1
                     else:
@@ -90,7 +82,7 @@ def twitterpresentorno(link):
     twpresentornot = 0
     if twitterorno == 0:
         u = 1
-    if twitterorno == 1:
+    elif twitterorno == 1:
         #print(thetwittertext)
         for p in twitterusers:
             p = p.replace("@","")
@@ -98,7 +90,7 @@ def twitterpresentorno(link):
                 user_timeline = twitter.get_user_timeline(screen_name=p, count=1000)
             except TwythonError as e:
                 #print(e)
-                pass
+                pass # TODO shouldn't be ignoring exceptions.
             for tweets in user_timeline:
                 formattedtweets = str(tweets['text'].encode('utf-8')).replace("b\'","").replace("\'","")
                 formattedtweets = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', formattedtweets)
@@ -107,55 +99,63 @@ def twitterpresentorno(link):
                     break
     else:
         return1 = 1
-    
-    if twpresentornot == 1:
-        return1 = 1
-    else:
-        return1 = 0
-    #print(return1)
-    if u == 1:
-        #print('True')
-        return(True)
-    elif return1 == 0:
-        #print('False')
-        return(False)
-    else:
-        #print('False')
-        return(True)
 
-def verifiedlinks(add_website_here):
+    return1 = 1 if twpresentornot == 1 else  0
+
+    if u == 1 or return1 != 0:
+        return True
+    else:
+        return False
+
+def verified_links( url ):
+    """
+    Use's Web of Trust's API to detect untrustworthy web addresses.
+    """
+    Check if web address
     import requests
-    url = "http://api.mywot.com/0.4/public_link_json2"
+    MIN_TRUST_SCORE = 70
+    mywot_api_endpoint = "http://api.mywot.com/0.4/public_link_json2"
     #add_website_here = "https://www.ncbi.nlm.nih.gov/pubmed/26389314"
-    querystring = {"hosts":"/"+add_website_here+"/","callback":"process","key":"e38619a0411ceaa1021f883730ffeeae3a386fa0"}
+    querystring = {"hosts":"/"+ url + "/","callback":"process","key":"e38619a0411ceaa1021f883730ffeeae3a386fa0"}
     payload = ""
     headers = {
         'content-type': "application/x-www-form-urlencoded",
         'cache-control': "no-cache",
         'postman-token': "93ffde57-c70f-a775-d5ce-03f8e152e9da"
         }
-    response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+    response = requests.request("GET", mywot_api_endpoint, data=payload, headers=headers, params=querystring)
     data = response.text.replace("process","")
-    #print(data)
-    wot = int(data.split("[")[1].split(",")[0])
-    if wot > 70:
-        return("verified")
+    web_of_trust_score = int(data.split("[")[1].split(",")[0])
+    if web_of_trust_score > MIN_TRUST_SCORE:
+        return "verified"
     elif "blacklists" in data:
-        return("Blacklisted")
+        return "Blacklisted"
     else:
-        return("not verified")
+        return "not verified" # TODO Why are we returning Strings and not booleans here?
 
 def summarization(url):
+    """
+    Uses Aylien's Text Summarization API to summarize the text content of the
+    page at the given URL.
+    Returns the summarized text as a string.
+    """
     from aylienapiclient import textapi
     summarizedtext = ""
     client = textapi.Client("357c1a5a", "458f8b52cec9fdbfd260a52d728c9d80")
-    
+
     summary = client.Summarize({'url': url, 'sentences_number': 3})
     for sentence in summary['sentences']:
         summarizedtext = summarizedtext + " " + sentence;
-    return(summarizedtext)
+    return summarizedtext
 
-def urltitle(link):
+def url_title(link):
+    """
+    Uses IBM Watson AlchemyLanguage API to extract the title of the webpage at the
+    address of the link passed as argument.
+    Eg: link : "http://techcrunch.com/2016/01/29/ibm-watson-weather-company-sale/"
+        returns ->
+        title: "IBM Closes Weather Co. Purchase, Names David Kenny New Head Of Watson Platform"
+    """
     import json
     from watson_developer_cloud import AlchemyLanguageV1
     alchemy_language = AlchemyLanguageV1(api_key='899037d290dbf55145ab97ebccaae88d68b84210')
@@ -163,15 +163,17 @@ def urltitle(link):
     data = json.loads(alchemyres)
     return(data["title"])
 
-def otherlinks(urllink):
-    #from bs4 import BeautifulSoup
+def other_links(url):
+    """
+    Uses Microsoft's Cognitive API to evaluate the quality of a webpage, and suggest
+    better information if possible.
+    """
     from urllib.request import urlopen
     vorno=0
-    #urllink = "https://www.cancer.gov/about-cancer/treatment/cam/patient/cannabis-pdq"
-    
-    if(verifiedlinks(urllink) == "not verified"):
 
-        st = urltitle(urllink)
+    if(verified_links(url) == "not verified"):
+
+        st = url_title(url)
         import http.client, urllib.request, urllib.parse, urllib.error, base64
         headers = {
             # Request headers
@@ -184,24 +186,24 @@ def otherlinks(urllink):
             data = response.read()
             #print(data)
             data = json.loads(data.decode("utf-8"))
-            for h in data['webPages']['value']:
-                if h['displayUrl'] == urllink:
+            for h in data['webPages']['value']: # TODO variable names here are non-descriptive
+                if h['displayUrl'] == url:
                     pass
                 else:
-                    urlscores = verifiedlinks(h['displayUrl'])
+                    urlscores = verified_links(h['displayUrl'])
                     if urlscores == "verified":
                         v = 1
                         kk = "Non verified. Better Verified Info is : " + " "+summarization(h['displayUrl'])
                         return(kk)
-                        break;           
+                        break;
             if v == 0:
                 return("no verified links")
             conn.close()
         except Exception as e:
             print("[Errno {0}] {1}".format(e.errno, e.strerror))
     else:
-        return verifiedlinks(urllink)    
-   
+        return verified_links(url)
+
 def main(link):
     #link = "http://i.imgur.com/walokrp.png"
     tokens = [urllib.parse.urlparse(url) for url in ("",link)]
@@ -214,30 +216,13 @@ def main(link):
             else:
                 count = count + 1
         else:
-            if ".jpg" in link:
-                p = detectAdultContent("{\"url\":\"" + link+"\"}")
-                #print(p)
-                p = p & twitterpresentorno(link)
-                #print("p1=")
-                #print(type(p))
-                if p:
-                    return("Verified")
+            if ".jpg" in link or ".png" in link:
+                if no_adult_content("{\"url\":\"" + link+"\"}") and twitter_present(link):
+                    return "Verified"
                 else:
-                    return("Not Verified")
-                
-            elif ".png" in link:
-                p = detectAdultContent("{\"url\":\"" + link+"\"}")
-                #print(p)
-                p = p & twitterpresentorno(link)
-                #print("p2=")
-                #print(type(p))
-                if p:
-                    return("Verified")
-                else:
-                    return("Not Verified")
+                    return "Not Verified"
             else:
-                return(otherlinks(link))
-            
+                return other_links(link)
+
 if __name__ == "__main__":
     print(main('https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-0/p480x480/15094286_1461068273945394_240413192541301870_n.jpg?oh=2213d25515dac7200efbc93ec5abe94d&oe=58C33895'))
-    
