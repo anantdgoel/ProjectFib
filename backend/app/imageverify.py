c2f0d1d3a8db44b4ef7847dd7f2e3e05e53b119b
Micheal Waltz <ecliptik@gmail.com>
2016-11-21 14:58:48 -0800

    Use environment vars for config and set defaults if none exist

--- backend/app/imageverify.py
+++ backend/app/imageverify.py
@@ -4,24 +4,25 @@ import urllib
 import urllib.error
 import urllib.parse
 import urllib.request
+import os
 
 #TODO Move all these secret keys to a single file. Maybe use a ConfigParser for this?
-MICROSOFT_CV_SUBSCRIPTION_KEY = '92e7b0d9a88a4a6495c5b40481cbe81e'
+MICROSOFT_CV_SUBSCRIPTION_KEY = os.getenv('MICROSOFT_CV_SUBSCRIPTION_KEY', "92e7b0d9a88a4a6495c5b40481cbe81e")
 
-TWITTER_API_KEY = 'WwA5L9U5PCqbnlfblKwKF0LEo'
-TWITTER_API_SECRET = 'ezUnYWFGyoOzeW68Au7MWLbYvX8ashIYWMEPtOnMqmxQgzXlRu'
-OAUTH_TOKEN = '136941431-Fwtibpyy072k8bqnoJyvLyrid69ZnUIYqbgPGiFr'
-OAUTH_TOKEN_SECRET = 'mtkQ6H6XFElMmp6YV3fLlwx6tPBsZLxt39VWvAJ5H3EJY'
+TWITTER_API_KEY = os.getenv('TWITTER_API_KEY', "WwA5L9U5PCqbnlfblKwKF0LEo")
+TWITTER_API_SECRET = os.getenv('TWITTER_API_SECRET', "ezUnYWFGyoOzeW68Au7MWLbYvX8ashIYWMEPtOnMqmxQgzXlRu")
+OAUTH_TOKEN = os.getenv('OAUTH_TOKEN', "136941431-Fwtibpyy072k8bqnoJyvLyrid69ZnUIYqbgPGiFr")
+OAUTH_TOKEN_SECRET = os.getenv('OAUTH_TOKEN_SECRET', "mtkQ6H6XFElMmp6YV3fLlwx6tPBsZLxt39VWvAJ5H3EJY")
 
-WOT_API_KEY = "e38619a0411ceaa1021f883730ffeeae3a386fa0"
-MIN_TRUST_SCORE = 70
+WOT_API_KEY = os.getenv('WOT_API_KEY', "e38619a0411ceaa1021f883730ffeeae3a386fa0")
+MIN_TRUST_SCORE = int(os.getenv('MIN_TRUST_SCORE', 70))
 
-AYLIEN_APP_ID = "357c1a5a"
-AYLIEN_APP_KEY = "458f8b52cec9fdbfd260a52d728c9d80"
+AYLIEN_APP_ID = os.getenv('AYLIEN_APP_ID', "357c1a5a")
+AYLIEN_APP_KEY = os.getenv('AYLIEN_APP_KEY', "458f8b52cec9fdbfd260a52d728c9d80")
 
-IBM_WATSON_API_KEY = '899037d290dbf55145ab97ebccaae88d68b84210'
+IBM_WATSON_API_KEY = os.getenv('IBM_WATSON_API_KEY', "899037d290dbf55145ab97ebccaae88d68b84210")
 
-MICROSOFT_SEARCH_SUBSCRIPTION_KEY = '71b952e8431b4059b3eef47c50eead89'
+MICROSOFT_SEARCH_SUBSCRIPTION_KEY = os.getenv('MICROSOFT_SEARCH_SUBSCRIPTION_KEY', "71b952e8431b4059b3eef47c50eead89")
 
 def no_adult_content(body):
     """
@@ -118,7 +119,7 @@ def verified_links( url ):
     """
     Use's Web of Trust's API to detect untrustworthy web addresses.
     """
-    Check if web address
+    #Check if web address
     import requests
     mywot_api_endpoint = "http://api.mywot.com/0.4/public_link_json2"
     #add_website_here = "https://www.ncbi.nlm.nih.gov/pubmed/26389314"
