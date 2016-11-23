cc281b8da0f41e9f6f5a13cc956368b5b5c68748
Mark Craft <markshocraft@gmail.com>
2016-11-23 18:58:30 -0500

    Slight fixes

--- extension/myScript.js
+++ extension/myScript.js
@@ -1,53 +1,74 @@
-
 /**
  * @file myScript.js
- * Script to analyze Facebook feed and make connection with the server ;
+ * Script to analyze Facebook feed and make connection with the server
  *
- * @author Mark Craft, Qinglin Chen
  * @date Fall 2016
  */
+(function(document) {
 
 var feeds = new Set();
-
-(function(document) {
 
 /**
  * Http request to fbserve.herokuapp.com.
  *
- * @param the text or url to send to the server.
+ * @param url to send to the server.
+ * @param the type of information sent
+ * @param the location to put the button
  */
 function httpGet(input, type, data) {
 
 	var server = "https://fbserve.herokuapp.com/";
 	var contents = "?content=";
-	
-	var page;
-	if(type=="url") page = decode(input);
-	else page = input;
-
-	//page.replace("http:", "http://");
-
-    //debugger;
-	console.log(page);
-
+	var page = (type=="url")? decode(input) : input;
 	var theUrl = server + contents + page;
 	theUrl = theUrl.replace("&", "^");
+	
+	//console.log("Type: " + type + " : " + page);
 
 	fetch(theUrl)
-		.then(function(res)
-		{ return res.text(); })
-		.then(function(text)
-		{
-				var div = document.createElement('div'),
-					button = Ladda.create(div);
-				data.appendChild(div);
-
-				if(text=="verified") div.innerHTML = "verified";
-				else div.innerHTML = "not verified";
-
-				if(text=="verified") div.style = "front-weight:bold; padding: 3px; position:absolute; top: 4px; right: 30px;background: #3b5998; font-size: 15px; color: #D5F5E3;";
-				else div.style = "front-weight:bold; padding: 3px; position:absolute; top: 4px; right: 30px;background: #3b5998; font-size: 15px; color: #E74C3C;";
+		.then(function(res) { 
+			return res.text();
+		}).then(function(text) {
+			var btn = document.createElement('div'),
+				button = Ladda.create(btn);
+			btn.style = "front-weight:bold; padding: 3px; position:absolute; top: 4px; right: 30px;background: #3b5998; font-size: 15px;";
+			if(text=="verified") {
+				btn.innerHTML = "verified";
+				btn.style.color = "#D5F5E3";
+			} else {
+				btn.innerHTML = "not verified";
+				btn.style.color = "#E74C3C";
+			}
+			data.appendChild(btn);
 		});
+	
+}
+
+/**
+ * Create a button on the screen
+ *
+ * @param location of the button
+ * @param the text to display on the button
+ * @param whether the server is down or not
+ */
+function createButton(btn, loc) {
+	btn = document.createElement('div'),
+		button = Ladda.create(btn);
+	//btn.addEventListener("mouseover", hoverTooltip(text), false);
+
+	btn.innerHTML = "server down";
+	btn.style = "front-weight:bold; padding: 3px; position:absolute; top: 4px; right: 30px;background: #3b5998; font-size: 15px; color: #FFFFFF;";
+
+	loc.appendChild(btn);
+}
+
+/**
+ * Display tooltip with more accurate information
+ *
+ * @param the information to display
+ */
+function hoverTooltip(info) {
+	//console.log("hovering: " + info);
 }
 
 /*
@@ -67,8 +88,6 @@ function decode(code) {
 	res = res.replace(end, "");
 	var end2 = res.substr(res.indexOf("&"), res.length);
 	res = res.replace(end2, "");
-
-	//console.log(res);
 
 	return res;
 }  
@@ -97,47 +116,53 @@ setInterval(function() {
 
 			var processed = false;
 
-			
 			var linked = test[i].querySelector('._6ks');
 			if(!processed && linked != null && linked.querySelector('a')!=undefined) {
-				//console.log(linked.querySelector('a'));
+				processed = true;
 				httpGet(linked.querySelector('a').href, "url", data);
-				processed = false;
 			}
 
 	
 			var link = test[i].querySelector('._5pbx.userContent');
 			if(!processed && link != null && link.querySelector('a') != null && link.querySelector('a').href!=undefined) {
-				//console.log(link);
+				processed = true;
 				httpGet(link.href, "url", data);
 			}
             
 
 			var picComment = test[i].querySelector('.uiScaledImageContainer._4-ep');
-
-			if(picComment != null && picComment.querySelector('img') != undefined && picComment.querySelector('img').src!=undefined) {
-
-				//console.log(picComment);
+			if(!processed && picComment != null && picComment.querySelector('img') != undefined && picComment.querySelector('img').src!=undefined) {
+				processed = true;
 				httpGet(picComment.querySelector('img').src, "image", data);
-				processed = false;
 			}
 
 			var picPost = test[i].querySelector('._46-h._517g');
 			if(!processed && picPost != null  &&  picPost.querySelector('img') != undefined && picPost.querySelector('img').src!=undefined) {
-				//console.log(picPost);
+				processed = true;
 				httpGet(picPost.querySelector('img').src, "image", data);
-				proccessed = false;
 			}
 			
+			var picTagged = test[i].querySelector('._4-eo._2t9n');
+			if(!processed && picTagged != null && picTagged.querySelector('._46-h._4-ep') != undefined && picTagged.querySelector('._46-h._4-ep').querySelector('img') != undefined) {
+				processed = true;
+				httpGet(picTagged.querySelector('._46-h._4-ep').querySelector('img').src, "image", data);
+			}
+
+			/*
+			var picAlbum = test[i].querySelector('._2a2q');
+			if(!processed && picAlbum != null && picAlbum.querySelectorAll('._5dec._xcx')!=undefined) {
+				processed = true;
+				var pics = picAlbum.querySelectorAll('a._5dec._xcx');
+				for(int i=0; i<pics.length; i++) 
+			}
+			*/
+
 			var text = test[i].querySelector('._5pbx.userContent');
 			if(!processed && text != null && text.textContent!=undefined) {
-				//console.log(text);
+				processed = true;
 				httpGet(text.textContent, "text", data);
-				processed = false;
 			}
 	
-		} else {
-			//console.log("have feed");
 		}
 	}
 
